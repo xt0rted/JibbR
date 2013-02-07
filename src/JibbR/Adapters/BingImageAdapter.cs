97798e5822d737fddb84e75b6deb69bfe5906689
Brian Surowiec <xtorted@optonline.net>
2013-02-07 11:38:55 -0500

    split the bing commands out into two adapters

---
+++ src/JibbR/Adapters/BingImageAdapter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+using Newtonsoft.Json.Linq;
+
+namespace JibbR.Adapters
+{
+    [RobotAdapterMetadata("bing-image-adapter")]
+    public class BingImageAdapter : IRobotAdapter
+    {
+        private readonly IBingClient _bingClient;
+        private readonly BingAdapterSettings _settings;
+
+        public BingImageAdapter(ISettingsManager settingsManager, IBingClient bingClient)
+        {
+            _bingClient = bingClient;
+
+            _settings = settingsManager.LoadSettings()
+                                       .For<BingImageAdapter>()
+                                       .ToObject<BingAdapterSettings>();
+
+            if (string.IsNullOrWhiteSpace(_settings.ApiKey))
+            {
+                Console.Error.WriteLine("No API key was found for bing. If you do not have one you can get one by signing up here http://www.bing.com/developers/");
+            }
+        }
+
+        public void Setup(IRobot robot)
+        {
+            robot.AddListener(@"^(bing )?image( me)? (?<query>.*)", (session, message, room, match) =>
+            {
+                var query = match.Groups["query"].Value;
+
+                var result = _bingClient.ImageSearch(_settings.ApiKey, query);
+
+                if (result.StartsWith("{"))
+                {
+                    var results = JObject.Parse(result);
+                    var imageUrl = results["d"]["results"].Select(x => (string)x["MediaUrl"]).RandomElement(new Random());
+
+                    session.Client.Send(imageUrl, room);
+                }
+            });
+        }
+    }
+}
---
+++ src/JibbR/Adapters/BingWebAdapter.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace JibbR.Adapters
+{
+    [RobotAdapterMetadata("bing-web-adapter")]
+    public class BingWebAdapter : IRobotAdapter
+    {
+        private readonly IBingClient _bingClient;
+
+        public BingWebAdapter(IBingClient bingClient)
+        {
+            _bingClient = bingClient;
+        }
+
+        public void Setup(IRobot robot)
+        {
+            robot.AddResponder(@"bing\s+(me\s+)?(?<query>.*)", (session, message, room, match) =>
+            {
+                System.Threading.Tasks.Task.Factory.StartNew(() =>
+                {
+                    var query = match.Groups["query"].Value;
+
+                    var result = _bingClient.WebSearch(query);
+
+                    var resultMatch = Regex.Match(result, @"<div class=""sb_tlst""><h3><a href=""(?<result>[^""]*)");
+
+                    string resultMessage;
+                    if (resultMatch.Success)
+                    {
+                        resultMessage = string.Format("@{0} {1}", session.Message.User.Name, resultMatch.Groups["result"].Value);
+                    }
+                    else
+                    {
+                        resultMessage = string.Format("@{0} Sorry, Bing had zero results for '{1}'", session.Message.User.Name, query);
+                    }
+
+                    session.Client.Send(resultMessage, room);
+                });
+            });
+        }
+    }
+}
